455a15e81b2ce0ed6ca844ef4cc52e18eb9ed6df
Eric Jones <Eric.Jones@asynchrony.com>
2014-05-02 12:44:31 -0500

    added OCMock to the project

--- Effective-Spotify-Server/Effective-Spotify-ServerTests/Effective_Spotify_ServerTests.m
+++ Effective-Spotify-Server/Effective-Spotify-ServerTests/Effective_Spotify_ServerTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <OCMock/OCMock.h>
 
 @interface Effective_Spotify_ServerTests : XCTestCase
 
@@ -26,9 +27,5 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
 
 @end
